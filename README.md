b1a65f1a03332c4aad9940be39c85a7b36b931e1
djordjijeK <krivokapic.djordjije@gmail.com>
2021-11-05 10:57:43 +0100

    WIP

--- README.md
+++ README.md
@@ -3,15 +3,20 @@
 A **data structure** is a particular way of organizing data in a computer memory so that it can be used effectively.
 In other words, it is a collection of data, the relationships among them, and the functions or operations that can be applied to the data.
 
-## 1. Dynamic Array
+## 1. [Dynamic Array](src/main/java/DynamicArray.java)
 
-Dynamic array is used to store dynamically sized collection of elements. 
-Contrary to static arrays that are fixed in size, a dynamic array grows its size automatically when new elements are added to it.
+**Dynamic Array** is used to store dynamically sized collection of elements. 
+Contrary to static arrays (`int[] array = new int[5]`) that are fixed in size, a dynamic array grows its size automatically when new elements are added to it.
 In other words, it grows its size to accommodate new elements and shrinks the size when the elements are removed.
+All elements of the array are stored in contiguous memory locations.
 
 | Operation | Time Complexity | Space Complexity | Note |
 | :--: | :--: | :--: | :---: |
-| Seconds | O(1) | 283 | gasgasg |
+| Indexing | O(1) | O(1) | the cost of access to the elements |
+| Insert (at beginning) | O(n) | O(n) | due to element shifting |
+| Delete (at beginning) | O(n) | O(n) | due to element shifting |
+| Insert (at end) | O(1) | O(1) | when resizing is required, this operation takes O(n) time and O(n) space |
+| IndexOf | O(n) | O(1) | searches the element index |
 
 ## 2. Linked List
 
--- src/main/java/DynamicArray.java
+++ src/main/java/DynamicArray.java
@@ -5,7 +5,7 @@ public class DynamicArray<T> implements Iterable<T>
 {
     private T[] array;          // container
     private int length   = 0;   // current array length
-    private int capacity = 0;   // array capcity
+    private int capacity = 0;   // array capacity
 
     /**
      * DynamicArray constructor.
@@ -30,47 +30,50 @@ public class DynamicArray<T> implements Iterable<T>
     }
 
     /**
-     * Appends a new item at the end of an array.
+     * Appends a new element at the end of an array.
      * Time  Complexity: worst-case O(n), other O(1)
      * Space Complexity: worst-case O(n), other O(1)
      * 
-     * @param item - new item to be appended.
+     * @param element - new element to be appended.
      */
-    public void append(T item)
+    public void append(T element)
     {
-        // we need to resize the array
+        // we need to resize the array ~ O(n)
         if (this.length + 1 >= this.capacity)
         {
             this.capacity = this.capacity == 0 ? 1 : this.capacity * 2;     // double the capacity
 
             T[] new_array = (T[]) new Object[this.capacity];
-            for (int i = 0; i < this.length; i++) new_array[i] = this.array[i];
+            for (int i = 0; i < this.length; i++) 
+                new_array[i] = this.array[i];
 
             this.array = new_array;
         }
 
-        this.array[this.length++] = item;
+        this.array[this.length++] = element;
     }
 
     /**
-     * Returns index of an item in the array.
+     * Returns the index of an element in the array.
      * Time  Complexity: O(n)
      * Space Complexity: O(1)
      * 
-     * @param item - item in the array.
-     * @return int - index of an item in the array if exists, -1 otherwise.
+     * @param element - element in the array.
+     * @return int    - index of the element in the array if exists, -1 otherwise.
      */
-    public int indexOf(T item)
+    public int indexOf(T element)
     {
         for (int index = 0; index < this.length(); index++)
         {
-            if (item == null) 
+            if (element == null) 
             {
-                if (this.array[index] == null) return index;
+                if (this.array[index] == null) 
+                    return index;
             }
             else
             {
-                if (item.equals(this.array[index])) return index;
+                if (element.equals(this.array[index])) 
+                    return index;
             }
         }
 
@@ -78,12 +81,12 @@ public class DynamicArray<T> implements Iterable<T>
     }
 
     /**
-     * Removes an item from the specified index.
+     * Removes an element from the specified index.
      * Time  Complexity: O(n)
      * Space Complexity: O(n)
      * 
      * @param index - array index.
-     * @return T    - removed item at the specified index.
+     * @return T    - removed element at the specified index.
      */
     public T removeAt(int index)
     {
@@ -97,55 +100,56 @@ public class DynamicArray<T> implements Iterable<T>
         {
             if (i == index) 
                 j--;
-            else new_array[j] = this.array[i];
+            else 
+                new_array[j] = this.array[i];
         }
 
         this.capacity = --this.length;
-        this.array  = new_array;
+        this.array    = new_array;
 
         return data;
     }
 
     /**
-     * Removes an item from an array.
+     * Removes an element from an array.
      * Time  Complexity: O(n)
      * Space Complexity: O(n)
      * 
-     * @param item     - item in the array.
-     * @return boolean - true if item is removed, false otherwise.
+     * @param element  - element in the array.
+     * @return boolean - true if element is removed, false otherwise.
      */
-    public boolean remove(T item)
+    public boolean remove(T element)
     {
-        int index = this.indexOf(item);
+        int index = this.indexOf(element);
 
         if (index == -1) 
             return false;
         
         this.removeAt(index);
-        
+
         return true;
     }
 
     /**
-     * Checks if an item exists in an array.
+     * Checks if an element exists in the array.
      * Time  Complexity: O(n)
      * Space Complexity: O(1)
      * 
-     * @param item     - item in the array.
-     * @return boolean - true if item exists, false otherwise.
+     * @param element  - element in the array.
+     * @return boolean - true if element exists, false otherwise.
      */
-    public boolean contains(T item)
+    public boolean contains(T element)
     {
-        return this.indexOf(item) != -1;
+        return this.indexOf(element) != -1;
     }
 
     /**
-     * Returns an item at the specified index.
+     * Returns an element at the specified index.
      * Time  Complexity: O(1)
      * Space Complexity: O(1)
      * 
      * @param index - array index.
-     * @return T    - item at the specified index.
+     * @return T    - element at the specified index.
      */
     public T get(int index)
     {
@@ -156,23 +160,23 @@ public class DynamicArray<T> implements Iterable<T>
     }
 
     /**
-     * Sets an itemt at the specified index.
+     * Sets an element at the specified index.
      * Time  Complexity: O(1)
      * Space Complexity: O(1)
      * 
-     * @param index - array index.
-     * @param T     - item to be set.
+     * @param index   - array index.
+     * @param element - element to be set.
      */
-    public void set(int index, T item)
+    public void set(int index, T element)
     {
         if (index >= this.length || index < 0) 
             throw new IndexOutOfBoundsException();
 
-        this.array[index] = item;
+        this.array[index] = element;
     }
 
     /**
-     * Clears an array.
+     * Clears the array.
      * Time  Complexity: O(n)
      * Space Complexity: O(1)
      */
@@ -185,7 +189,7 @@ public class DynamicArray<T> implements Iterable<T>
     }
 
     /**
-     * Returns a dynamic array length.
+     * DynamicArray length.
      */
     public int length()
     {
@@ -193,7 +197,7 @@ public class DynamicArray<T> implements Iterable<T>
     }
 
     /**
-     * Returns a dynamic array capacity.
+     * DynamicArray capacity.
      */
     public int capacity()
     {
@@ -201,7 +205,7 @@ public class DynamicArray<T> implements Iterable<T>
     }
 
     /**
-     * Checks if an array is empty.
+     * Checks if the array is empty.
      */
     public boolean isEmpty()
     {
